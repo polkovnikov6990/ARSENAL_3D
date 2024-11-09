a6f17966cccbcef12678b996275c2749aba84319
Alexey <polkovnikov6990@mail.ru>
2024-11-10 02:44:10 +0500

    second

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,10 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/structure')
+def structure():
+    return render_template('structure.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
